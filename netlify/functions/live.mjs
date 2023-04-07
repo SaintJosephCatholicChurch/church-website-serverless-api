e1cd81d6cf1bb4a87a466638aff32d5375eeb8c9
Daniel Lautzenheiser <lautzd@gmail.com>
2023-04-07 01:08:56 -0400

    Update API

--- netlify/functions/live.mjs
+++ netlify/functions/live.mjs
@@ -1,7 +1,10 @@
 import fetch from "node-fetch";
+import { parse } from "node-html-parser";
+import { XMLParser } from 'fast-xml-parser';
 
-export const handler = async () => {
+export const handler = async (_event, _context) => {
   let isStreaming = false;
+  let url = "";
   try {
     const response = await fetch(
       `https://youtube.com/channel/UCEbcH06ZOVz8GhA8fbRYusg/live`
@@ -11,13 +14,24 @@ export const handler = async () => {
     const canonicalURLTag = html.querySelector("link[rel=canonical]");
     const canonicalURL = canonicalURLTag.getAttribute("href");
     isStreaming = canonicalURL.includes("/watch?v=");
+
+    if (isStreaming) {
+      url = canonicalURL;
+    } else {
+      const response2 = await fetch(
+        `https://www.youtube.com/feeds/videos.xml?channel_id=UCEbcH06ZOVz8GhA8fbRYusg&orderby=published`
+      );
+      const parser = new XMLParser();
+      let jObj = parser.parse(await response2.text());
+      url = `https://www.youtube.com/watch?v=${jObj.feed.entry[0]["yt:videoId"]}`;
+    }
   } catch (e) {
     console.error(e);
   }
 
   return {
     statusCode: 200,
-    body: { isStreaming },
+    body: { isStreaming, url },
     headers: {
       "access-control-allow-origin": "https://www.stjosephchurchbluffton.org",
     },
