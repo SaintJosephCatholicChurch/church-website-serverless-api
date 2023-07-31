ba254021d5e9a3e5a1cda9e152b8514bd8b4536f
Daniel Lautzenheiser <lautzd@gmail.com>
2023-07-31 11:31:41 -0400

    Update contact.mjs

--- netlify/functions/contact.mjs
+++ netlify/functions/contact.mjs
@@ -13,7 +13,7 @@ export const handler = async () => {
 
   const info = await transporter.sendMail({
     from: 'no-reply@stjosephchurchbluffton.org',
-    to: "test-144ff9@test.mailgenius.com",
+    to: "test-94e2f5@test.mailgenius.com",
     subject: "New Contact Form Submission",
     html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
