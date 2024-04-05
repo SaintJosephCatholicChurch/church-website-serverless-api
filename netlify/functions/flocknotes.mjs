7fd3605b5a0d7aac7e66d94dd0c86e21b0e767f7
Daniel Lautzenheiser <lautzd@gmail.com>
2024-04-05 12:42:25 -0400

    Create flocknotes.mjs

---
+++ netlify/functions/flocknotes.mjs
@@ -0,0 +1,22 @@
+import fetch from "node-fetch";
+
+export const handler = async () => {
+  const response = await fetch("https://rss.flocknote.com/852042");
+  if (!response.ok) {
+    return {
+      statusCode: 500,
+      body: "",
+      headers: {
+        "access-control-allow-origin": "https://www.stjosephchurchbluffton.org",
+      },
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: await response.text(),
+    headers: {
+      "access-control-allow-origin": "https://www.stjosephchurchbluffton.org",
+    },
+  };
+};
