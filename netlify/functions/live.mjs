c20bf0626a20b5f976c655ccdc0ff43c465e47d4
Daniel Lautzenheiser <lautzd@gmail.com>
2024-04-20 11:03:01 -0400

    Update live.mjs

--- netlify/functions/live.mjs
+++ netlify/functions/live.mjs
@@ -79,7 +79,6 @@ export const handler = async (event) => {
           "--no-zygote",
           "--use-gl=swiftshader",
           "--window-size=1920,1080",
-          "--single-process",
           "--disable-gpu",
           "--disable-dev-shm-usage",
           "--no-first-run",
