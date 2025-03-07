29db2fd9af03801e0bbe08ebdd3c64f1094f3c69
Daniel Lautzenheiser <lautzd@gmail.com>
2025-03-07 08:11:44 -0500

    Update readings-podcast.mjs

--- netlify/functions/readings-podcast.mjs
+++ netlify/functions/readings-podcast.mjs
@@ -18,9 +18,23 @@ export const handler = async () => {
     };
   }
 
+  const text = await response.text();
+  const match = /<iframe (?:[^>]+) (?:src=")(https:\/\/w\.soundcloud\.com\/player[^"]+)" (?:[^>]+)>/.exec(text);
+  if (!match || !match.length !== 2) {
+    return {
+      statusCode: 500,
+      body: '',
+      headers: {
+        'access-control-allow-origin': 'https://www.stjosephchurchbluffton.org',
+      },
+    };
+  }
+
   return {
     statusCode: 200,
-    body: await response.text(),
+    body: {
+      url: match[1],
+    },
     headers: {
       'access-control-allow-origin': 'https://www.stjosephchurchbluffton.org',
     },
