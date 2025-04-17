340f281439af3fcc07eadb2c555b342fddfa5e0b
Daniel Lautzenheiser <lautzd@gmail.com>
2025-04-17 08:29:14 -0400

    Update readings-podcast.mjs

--- netlify/functions/readings-podcast.mjs
+++ netlify/functions/readings-podcast.mjs
@@ -1,7 +1,39 @@
 import fetch from 'node-fetch';
 import { format } from 'date-fns';
 
-export const handler = async () => {
+function getLectionaryYear(date = new Date()) {
+  const liturgicalYear = getLiturgicalYear(date);
+  const cycle = liturgicalYear % 3;
+
+  if (cycle === 1) {
+    return 'year-a';
+  }
+
+  if (cycle === 2) {
+    return 'year-b';
+  }
+
+  return 'year-c';
+}
+
+// Helper: Get Liturgical Year
+function getLiturgicalYear(date = new Date()) {
+  const year = date.getFullYear();
+  const christmas = new Date(year, 11, 25);
+  const adventSunday = getFirstAdventSunday(christmas);
+  return date >= adventSunday ? year : year - 1;
+}
+
+// Helper: Find First Sunday of Advent
+function getFirstAdventSunday(christmas) {
+  const fourthSunday = new Date(christmas);
+  fourthSunday.setDate(christmas.getDate() - 28);
+  const dayOfWeek = fourthSunday.getDay();
+  fourthSunday.setDate(fourthSunday.getDate() - dayOfWeek); // Back to Sunday
+  return fourthSunday;
+}
+
+async function getPodcastUrl() {
   const podcastsResponse = await fetch(`https://bible.usccb.org/podcasts/audio`);
   if (!podcastsResponse.ok) {
     return {
@@ -14,6 +46,16 @@ export const handler = async () => {
   }
 
   const podcastPageText = await podcastsResponse.text();
+  const podcastPageMatchWithYear = new RegExp(
+    `<a href="(\/podcasts\/audio\/daily-mass-reading-podcast-${format(
+      new Date(),
+      'MMMM-d-yyyy'
+    ).toLowerCase()}-${getLectionaryYear()}[a-zA-Z0-9_-]+)">`
+  ).exec(podcastPageText);
+  if (podcastPageMatch && podcastPageMatch.length === 2) {
+    return podcastPageMatchWithYear[1];
+  }
+
   const podcastPageMatch = new RegExp(
     `<a href="(\/podcasts\/audio\/daily-mass-reading-podcast-${format(
       new Date(),
@@ -30,7 +72,16 @@ export const handler = async () => {
     };
   }
 
-  const response = await fetch(`https://bible.usccb.org${podcastPageMatch[1]}`);
+  return podcastPageMatch[1];
+}
+
+export const handler = async () => {
+  const podcastUrl = await getPodcastUrl();
+  if (podcastUrl !== string) {
+    return podcastUrl;
+  }
+
+  const response = await fetch(`https://bible.usccb.org${podcastUrl}`);
   if (!response.ok) {
     return {
       statusCode: 500,
